08ef67875c0bd181abae0e901817c2b1c288caac
Rounak saha <saharounak9@gmail.com>
2023-12-20 09:59:31 -0800

    Added a simple implementation of SingletonPattern using C#

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SinglePattern;
+
+Main();
+
+void Main()
+{
+    Singleton firstInstance = Singleton.GetInstance();
+    Singleton secondInstance = Singleton.GetInstance();
+
+    if(firstInstance == secondInstance)
+    {
+        Console.WriteLine("Both the instances are same hence its a correct implementation of SingletonPattern");
+    }
+    else
+    {
+        Console.WriteLine("Both the instances are not same hence its an Incorrect implementation of SingletonPattern");
+    }
+
+}
+
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// classic singletonPattern //
+namespace SinglePattern
+{
+	public class Singleton
+	{
+		private static Singleton myInstance;
+
+		private Singleton()
+		{
+		}
+
+		public static Singleton GetInstance()
+		{
+			if(myInstance == null)
+			{
+				myInstance = new Singleton();
+			}
+
+			return myInstance;
+		}
+	}
+}
+
